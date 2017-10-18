7ca00749cdbce3419c5429cfc1e27a0d0f212c62
nursh <nsheikhmohammed@yahoo.com>
2017-10-18 10:07:22 -0400

    created firebase config file and removed multer from server

--- app.js
+++ app.js
@@ -6,22 +6,12 @@ const ms = require('ms');
 const compression = require('compression');
 const bodyParser = require('body-parser');
 // const expressStaticGzip = require('express-static-gzip');
-const multer = require('multer');
 require('dotenv').config();
 const recipes = require('./assets.json');
 
 
 const port = process.env.PORT || 3000;
 const app = express();
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, './upload');
-  },
-  filename(req, file, cb) {
-    cb(null, `${file.originalname}`);
-  },
-});
-const upload = multer({ storage });
 
 app.use(helmet());
 app.use(helmet.hsts({
@@ -34,7 +24,6 @@ app.use(express.static(files));
 // app.use('/', expressStaticGzip(files));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.use(fileUpload());
 
 
 app.get('/api/recipe/:recipeId', (req, res) => {
@@ -49,11 +38,6 @@ app.get('/api/recipes', (req, res) => {
 
 app.post('/recipify', (req, res) => {
   res.status(200).send({ recipe: req.body });
-});
-
-app.post('/upload', upload.single('imageFile'), (req, res) => {
-  console.log(req.body.id);
-  res.status(200).send({ success: 'success' });
 });
 
 app.get('*', (req, res) => {
